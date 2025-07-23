7cf8de805ba833c9ce1b62e7ad77969d4b15b948
Lakshmi Lavanya <121945454+Lavanya573@users.noreply.github.com>
2025-07-23 14:01:59 +0530

    Update settings.py

--- tripease_backend/settings.py
+++ tripease_backend/settings.py
@@ -24,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pf^p8*gatgan#lyt^nf=w0x!5zd%s*k&cu+#hg&(9stk8hu7bw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#
-DEBUG = False
-# DEBUG = True
-ALLOWED_HOSTS = ['Lakshmimunukutla.pythonanywhere.com']
+#DEBUG = False
+DEBUG = True
+# ALLOWED_HOSTS = ['Lakshmimunukutla.pythonanywhere.com']
 
-# ALLOWED_HOSTS=[]
+ALLOWED_HOSTS=[]
 # Application definition
 
 INSTALLED_APPS = [
